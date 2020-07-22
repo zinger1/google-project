c1ff2a2c407eb09aa89c4d6f6f31f3fb9d601416
Miriam <zm@sem-wolf.org>
2020-07-22 13:41:40 +0300

    get_best_k_completions function work on prefix that exist in about.txt file

--- auto_complete.py
+++ auto_complete.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import json
 data = defaultdict(set)
+list_data_sentences = []
 
 class AutoCompleteData:
     def __init__(self, completed_sentence, source_text, offset, score):
@@ -8,30 +9,53 @@ class AutoCompleteData:
         self.source_text = source_text
         self.offset = offset
         self.score = score
+
     def get_completed_sentence(self):
         return self.completed_sentence
+
     def get_score(self):
         return self.score
+
     def set_score(self, num):
         self.score -= num
+
 def initialized_data():
+
+    offset = 0
     data_file = open("about.txt")
     data_sentences = data_file.read().split("\n")
+
+    
     for sentence in data_sentences:
+        
         if sentence:
-            for i in range(len(sentence)):
-                for j in range(i + 1, len(sentence) + 1):
-                    data[sentence[i: j]].add(AutoCompleteData(sentence, "file.txt", i, 2*len(sentence[i: j])))
             
-    with open("data.json", "w") as f:
-        for key in data.keys():
-            for a in data[key]:
-                json.dump({key:a.get_completed_sentence()}, f)
+            for i in range(len(sentence)):
+
+                for j in range(i + 1, len(sentence) + 1):
+                    new_data = AutoCompleteData(sentence, "file.txt", offset, 2*len(sentence[i: j]))
+
+                    data[sentence[i: j]].add(offset)
+
+            list_data_sentences.append(new_data)
+            offset += 1
+
+    # for item in data:
+    #     print(data[item])
+
+    # with open("data.json", "w") as f:
+
+    #     for key in data.keys():
+        
+    #         for a in data[key]:
+    #             json.dump({key:a.get_completed_sentence()}, f)
 
 def earase_prefix(prefix):
     earased_prefix =  []
     for i in range(len(prefix)):
+
         substring_for_search = data.get(prefix.replace(prefix[i], "", 1))
+        
         if(substring_for_search):
             substring_for_search.set_score(10 - 2 * i + 2) if i < 4 else substring_for_search.set_score(2 + 2)
             earased_prefix.append(substring_for_search)
@@ -39,19 +63,26 @@ def earase_prefix(prefix):
     
 def change_prefix(prefix):
     changed_prefix =  []
+
     for i in range(len(prefix)):
+    
         for letter in range(ord('a'), ord('z') + 1):
             substring_for_search = data.get(prefix.replace(prefix[i], letter, 1))
+    
             if substring_for_search:
                 substring_for_search.set_score(5 - i + 2) if i < 4 else substring_for_search.set_score(2 + 2)
                 changed_prefix.append(substring_for_search)
+
     return changed_prefix
 
 def add_prefix(prefix):
     added_prefix = []
+
     for i in range(len(prefix) + 1):
+    
         for letter in range(ord('a'), ord('z') + 1):
             substring_for_search = data.get(prefix[:i] + letter + prefix[i:])
+    
             if substring_for_search:
                 substring_for_search.set_score(5 - i + 2) if i < 4 else substring_for_search.set_score(2 + 2)
                 changed_prefix.append(substring_for_search)
@@ -64,11 +95,15 @@ def complete_prefix(prefix):
     return completed_prefix
 
 def get_best_k_completions(prefix):
-    founded_completions = data.get(prefix)
-    if len(founded_completions) < 5:
-        founded_completions += complete_prefix(prefix) 
-    founded_completions = sorted(founded_completions, key=lambda x: (x.get_score, x.get_completed_sentence))
+    founded_completions = [*data.get(prefix),]
+
+    # if len(founded_completions) < 5:
+    #     founded_completions += complete_prefix(prefix) 
+    founded_completions = sorted(list(map(lambda x: list_data_sentences[x], founded_completions)), key=lambda x: (x.get_score(), x.get_completed_sentence()))
+    # founded_completions = sorted(founded_completions, key=lambda x: (list_data_sentences[x].get_score(), x.get_completed_sentence()))
+    
     return founded_completions[:5]
+
 
 def menu():
     print("Loading the files and preparing the system...")
@@ -76,7 +111,8 @@ def menu():
     print("The system is ready. Enter your text:")
     prefix = input()
     best_completions_of_prefix = get_best_k_completions(prefix)
-    # for complete in best_completions_of_prefix:
-        # print(complete.get_completed_sentence, (, )) 
+    print(best_completions_of_prefix)
+    for complete in best_completions_of_prefix:
+        print(complete.completed_sentence, complete.offset, complete.score) 
 
 menu()
