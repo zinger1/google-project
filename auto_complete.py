61b348c39aa9e3f65c909ba7ac1ce820dcd442c4
Miriam <zm@sem-wolf.org>
2020-07-23 14:09:35 +0300

    Division into files

--- auto_complete.py
+++ auto_complete.py
@@ -1,3 +1,5 @@
+
+
 from collections import defaultdict
 from copy import deepcopy
 
@@ -53,86 +55,4 @@ def initialized_data():
             id_sentence += 1
         offset += 1
 
-def add_prefix(prefix):
-    added_prefix =  set()
 
-    for i in range(len(prefix)):
-        substring_for_search = data.get(prefix.replace(prefix[i], "", 1))
-        
-        if substring_for_search:
-            for index_of_auto_complete_data in substring_for_search:
-                added = deepcopy(list_data_sentences[index_of_auto_complete_data])
-                added.set_score(2*(len(prefix) -1) - (2 if i > 3 else 10 - 2*i))
-                added_prefix.add(added)
-    return added_prefix
-    
-def change_prefix(prefix):
-    changed_prefix = set()
-
-    for i in range(len(prefix)):
-        for letter in range(ord('a'), ord('z') + 1):
-            if chr(letter) not in prefix:
-                substring_for_search = data.get(prefix.replace(prefix[i], chr(letter), 1))
-                if substring_for_search:
-                    for index_of_auto_complete_data in substring_for_search:
-                        changed = deepcopy(list_data_sentences[index_of_auto_complete_data])
-                        changed.set_score(2*(len(prefix) -1) - (1 if i > 3 else 5 - i))
-                        changed_prefix.add(changed)
-    return changed_prefix
-
-def earase_prefix(prefix):
-    earased_prefix = set()
-
-    for i in range(len(prefix) + 1):
-        for letter in range(ord('a'), ord('z') + 1):
-            if chr(letter) not in prefix:
-                substring_for_search = data.get(prefix[:i] + chr(letter) + prefix[i:])
-                if substring_for_search:
-                    for index_of_auto_complete_data in substring_for_search:
-                        earased = deepcopy(list_data_sentences[index_of_auto_complete_data])
-                        earased.set_score(2*(len(prefix)) - (2 if i > 3 else 10 - 2*i))
-                        earased_prefix.add(earased)
-
-    return earased_prefix
-
-def complete_prefix(prefix):
-    completed_prefix = earase_prefix(prefix)
-    completed_prefix.update(change_prefix(prefix))
-    completed_prefix.update(add_prefix(prefix))
-    return completed_prefix
-
-def get_best_k_completions(prefix):
-    founded_completions = set()
-    if data.get(prefix): 
-        for item in data.get(prefix):
-            complete = list_data_sentences[item]  
-            complete.set_score(2 * len(prefix)) 
-            founded_completions.add(complete)
-        # founded_completions.update([list_data_sentences[item] for item in data.get(prefix)])
-    if len(founded_completions) < 5:
-        founded_completions.update(complete_prefix(prefix)) 
-    founded_completions = sorted( founded_completions, key=lambda x: (x.get_score(), x.get_completed_sentence()), reverse=True)
-    
-    return founded_completions[:5]
-
-
-def menu():
-    print("Loading the files and preparing the system...")
-    initialized_data()
-    print("The system is ready. Enter your text:")
-    prefix = input()
-    while(True):
-        if("#" == prefix[-1]):
-            prefix = input()
-
-        best_completions_of_prefix = get_best_k_completions(prefix_ignore_casing(prefix))
-        if best_completions_of_prefix:
-            for complete in best_completions_of_prefix:
-                print(complete.get_completed_sentence(), complete.get_source_text(), complete.get_offset(), complete.get_score())
-            print(prefix, end="")
-            prefix += input()
-        else:
-            print("There are no suggestions")
-            break
-
-menu()
